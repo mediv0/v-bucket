721508a6f9d8fb2a9a75ee288a9dfdbb5621a105
Mahdi Fakhr <36822136+MahdiFakhr@users.noreply.github.com>
2020-09-24 15:50:46 +0330

    :hammer: Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     parserOptions: {
         parser: "babel-eslint"
     },
+    ignorePatterns: ["dist"],
     rules: {
         "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
