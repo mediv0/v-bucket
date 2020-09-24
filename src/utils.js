60a26d792c9d384cb89f3fd91186016bb943652e
Mahdi Fakhr <36822136+MahdiFakhr@users.noreply.github.com>
2020-09-24 15:54:43 +0330

    :hammer: add project utilities

---
+++ src/utils.js
@@ -0,0 +1,50 @@
+import { InvalidModulePath } from "./Errors";
+
+export function isDefined(v) {
+    return v !== undefined && v !== null;
+}
+
+export function isPromise(fn) {
+    return (
+        isDefined(fn) &&
+        typeof fn.then === "function" &&
+        typeof fn.catch === "function"
+    );
+}
+export function parsePath(path) {
+    const splitted = path.split("/");
+    for (let i = 0; i < splitted.length; i++) {
+        if (splitted[i].length === 0) {
+            throw new InvalidModulePath(`
+                Invalid Path. we couldn't find any module in the ${path}`);
+        }
+    }
+    return splitted;
+}
+
+export function isPathAlreadyCalculated(v) {
+    return Array.isArray(v) && isString(v);
+}
+
+export function isString(arr) {
+    if (!(arr instanceof Array)) {
+        return false;
+    }
+    return arr.every(v => typeof v === "string");
+}
+
+export function arrayToPath(...path) {
+    return path.join("/");
+}
+
+export function isStringNotEmpty(str) {
+    return str.toString().trim().length > 0;
+}
+
+export function isObject(o) {
+    return o instanceof Object;
+}
+
+export function isObjectEmpty(o) {
+    return Object.keys(o).length === 0;
+}
