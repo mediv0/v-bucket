2b004e1bc5d75450221d8fc893c2280044429529
Mahdi Fakhr <36822136+MahdiFakhr@users.noreply.github.com>
2020-09-24 15:56:43 +0330

    :hammer: create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { Bucket, createBucket } from "./bucket";
+import { useBucket } from "./inject";
+
+export default {
+    createBucket,
+    Bucket,
+    useBucket
+};
+
+export { createBucket, Bucket, useBucket };
